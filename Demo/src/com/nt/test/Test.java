5e741563d846bc1b506947f302f4d2f12d8350a3
nayakar <tl@gmail.com>
2020-02-19 10:53:35 +0530

    2 nd commit

--- Demo/src/com/nt/test/Test.java
+++ Demo/src/com/nt/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("succesfully ediut");
 		System.out.println("welcome git hub");
+		System.out.println("welcome to 2nd time git hub");
 	}
 
 }
