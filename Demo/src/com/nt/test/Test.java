5245da4bd661d71d7ae7b2f197bc9de58271f2d6
nayakar <tl@gmail.com>
2020-02-19 12:42:11 +0530

    Tutorial

--- Demo/src/com/nt/test/Test.java
+++ Demo/src/com/nt/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		System.out.println("welcome git hub");
 		System.out.println("welcome to 2nd time git hub");
 		System.out.println("successfully 2nd time modified in github only");
-	
+	System.out.println("toturial ");
 		System.out.println("prasnna kumar");
 	
 	}
